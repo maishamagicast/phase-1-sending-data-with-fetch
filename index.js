b0bfc61ad3c485453398f854385ecaee57d9e48d
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-04-06 20:01:47 +0300

    sending data with fetch()

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { Body } = require('node-fetch');
+
 // Add your code here
+function submitData(username, email){
+   return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: username,
+            email: email
+        })
+    })
+    .then((response)=> response.json())
+    .then((data)=>{
+        console.log('Success:', data);
+        document.body.innerHTML =data.id;
+    })
+    .catch(function(error) {
+        const errorMessage = document.createElement('p');
+        errorMessage.innerHTML =error.message;
+        document.body.appendChild(errorMessage);
+    }
+       
+    )
+}
